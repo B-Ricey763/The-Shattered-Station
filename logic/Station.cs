8b2442b1f3f86b6d2c29caaba44fd1757800243a
B-Ricey763 <39998963+B-Ricey763@users.noreply.github.com>
2021-02-17 16:07:26 -0500

    Added tons of stuff

--- logic/Station.cs
+++ logic/Station.cs
@@ -4,72 +4,21 @@ using System.Collections.Generic;
 
 public class Station : Area2D
 {   
-    [Signal]
-    public delegate void HealthChanged();
-    [Export]
-    public float MaxHealth = 100.0f;
-
-    private List<ITower> _towers = new List<ITower>();
-
-    private float _health;
-
-    // Use a custom setter so health is within bounds
-    public float Health {
-        get => _health;
-        set {
-            _health = Mathf.Clamp(value, 0.0f, MaxHealth);
-            EmitSignal(nameof(HealthChanged), _health);
-        }
-    }
-
-    private Area2D _powerRange;
+    private Health _health;
 
     public override void _Ready()
     {   
-        _powerRange = GetNode<Area2D>("PowerRange");
-        // Give our starting health
-        Health = MaxHealth;
+        _health = GetNode<Health>("Health");
     }
 
-    private void BodyEntered(Node body)
+    public void BodyEntered(Node body)
     {
         if (body is Asteroid asteroid)
         {
             // We have been hit! Damage
-            Health -= asteroid.Damage;
+            _health.Value -= asteroid.Damage;
             // Delete the asteroid
             asteroid.QueueFree();
         }
-    }
-
-    public void BodyEnteredPower(Node body)
-    {
-        if (body.IsInGroup("tower"))
-            AddTower(body as RigidBody2D);
-    }
-
-    public void BodyExitedPower(Node body)
-    {
-        if (body.IsInGroup("tower"))
-            RemoveTower(body);
-    }
-
-    private void AddTower(RigidBody2D tower)
-    {
-        // The body is a tower, so we know it needs power
-        _towers.Add(tower);
-
-        // THIS IS TERRIBLE NEVER DO THIS AGAIN
-        Tether powerTether = tower.GetNode<Tether>("Tether");
-        // MY EYES ARE BLEEDING SOMEONE HELP ME
-        powerTether.Link(tower as Node2D, this);
-    }
-
-    private void RemoveTower(RigidBody2D tower)
-    {
-        _towers.Remove(tower);
-
-        Tether powerTether = (tower as Node2D).GetNode<Tether>("Tether");
-        powerTether.Unlink();
     }
 }
